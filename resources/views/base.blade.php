6146c9514a2f0f997a9ddff851f8b67a6cc42553
KelvinAarssen36 <117264110+KelvinAarssen36@users.noreply.github.com>
2023-11-28 14:34:01 +0100

    base.blade

---
+++ resources/views/base.blade.php
@@ -0,0 +1,30 @@
+  @yield('content')
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TicketStyeem</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Metamorphous&family=Monoton&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    </head>
+    <body>
+        <header>
+            <nav>
+            
+            </nav>
+        </header>
+        <main>
+            
+        </main>
+    </body>
+</html>

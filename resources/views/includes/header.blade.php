335b815e5764a76c4a9331ea51ef8d27107f330b
KelvinAarssen36 <117264110+KelvinAarssen36@users.noreply.github.com>
2023-12-05 15:44:25 +0100

    styling home page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,14 @@
 <header>
     <title>TicketShop</title>
     <span class="title">TicketShop</span>
-    <nav>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('events.index') }}">Events</a>
-        <a href="{{ route('schedule') }}">Agenda</a>
-    </nav>
+    <div class="between">
+        <nav>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('events.index') }}">Events</a>
+            <a href="{{ route('schedule') }}">Agenda</a>
+        </nav>
+        <nav>
+        <a class="login-button" href="{{ route('events.index') }}">Inloggen</a>
+        </nav>
+    </div>
 </header>

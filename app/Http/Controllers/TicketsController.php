0357979dc939f9777e0ae7fe724ad353227fa544
MartijnCurio <124584804+MartijnCurio@users.noreply.github.com>
2023-12-13 07:44:15 +0100

    Fixed TicketController use directories

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
-use app\Models\Ticket;
-use app\Models\Reservation;
+use App\Models\Ticket;
+use App\Models\Reservation;
 class TicketsController extends Controller
 {
 
